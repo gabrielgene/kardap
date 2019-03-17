9710f7b505ba6a1db619b3e832661ca89ad95dcb
Ramon Moraes <ramon.moraes94@gmail.com>
2019-03-17 20:51:54 -0300

    add comented post route

--- back/src/server.ts
+++ back/src/server.ts
@@ -2,6 +2,8 @@ import express from "express";
 import elasticsearch from "elasticsearch";
 
 const app = express();
+// app.use(express.json());
+
 const port = 8080; // default port to listen
 
 const client = new elasticsearch.Client({
@@ -22,8 +24,8 @@ app.get("/api/:index", async (req, res) => {
   };
 
   const fullQueryObject = requestQuery ? {
-  	...queryObject,
-  	body: { query: { match: requestQuery } },
+    ...queryObject,
+    body: { query: { match: requestQuery } },
   } : queryObject;
 
   try {
@@ -39,6 +41,44 @@ app.get("/api/:index", async (req, res) => {
     });
   }
 });
+
+// app.post("/api/:index/:id", async (req, res) => {
+//   const { index, id } = req.params;
+//   if (!id) {
+//     res.send({
+//       message: "id param is required",
+//       status: 400,
+//     });
+//   }
+
+//   const requestJson = req.body || null;
+//   if (!requestJson) {
+//     res.send({
+//       message: "invalid json",
+//       status: 400,
+//     });
+//   }
+//   res.send('oi')
+
+//   const elasticSearchJson = {
+//     body: requestJson,
+//     type: 'doc',
+//     index,
+//     id
+//   }
+
+//   try {
+//     const response = await client.create(elasticSearchJson);
+//     res.send({
+//       'message': 'ok'
+//     })
+//   } catch (err) {
+//     res.send({
+//       'message': 'err',
+//       'err': err
+//     });
+//   }
+// });
 
 // start the Express server
 app.listen(port, () => {
