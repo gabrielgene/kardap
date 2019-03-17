707dff9150012456b42cd6b9bd3919aaf5afe4d9
jocta.torres <jocta.torres@gmail.com>
2019-03-17 17:30:27 -0300

    Adding home routing

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,20 +1,26 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+
+import theme from './theme'
 
 import Category from './pages/category';
 import Restaurant from './pages/restaurant';
 import Serp from './pages/serp';
 import Home from './pages/home';
 
+
 const App = () => (
-  <Router>
-    <div>
-      <Route path="/" exact component={Home} />
-      <Route path="/categoria/:category" component={Category} />
-      <Route path="/restaurante/:slug" component={Restaurant} />
-      <Route path="/busca" component={Serp} />
-    </div>
-  </Router>
+  <MuiThemeProvider theme={theme} >
+    <Router>
+      <div>
+        <Route path="/" exact component={Home} />
+        <Route path="/categoria/:category" component={Category} />
+        <Route path="/restaurante/:slug" component={Restaurant} />
+        <Route path="/busca" component={Serp} />
+      </div>
+    </Router>
+  </MuiThemeProvider>
 );
 
 export default App;
--- front/src/pages/home/index.tsx
+++ front/src/pages/home/index.tsx
@@ -1,13 +1,20 @@
 import React from "react";
+import Button from '@material-ui/core/Button';
 
-const Home = () => {
+interface Props {
+  history: any
+}
+
+const Home = (props: Props) => {
   return (
     <React.Fragment>
       <h1>Home page</h1>
       <form action="">
         Busca: <input type='text' />
       </form>
-      <h2>Categoria</h2>
+      <Button variant="contained" color="secondary" onClick={() => props.history.push('/busca')}>
+        Categorias
+      </Button>
     </React.Fragment>
   );
 };
