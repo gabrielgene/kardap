f442d0a4bd139dd2f4bcfa4b8522ca0a1f972044
Ramon Moraes <ramon.moraes94@gmail.com>
2019-03-17 18:16:18 -0300

    server searching custom indexes

--- back/src/server.ts
+++ back/src/server.ts
@@ -5,34 +5,46 @@ const app = express();
 const port = 8080; // default port to listen
 
 const client = new elasticsearch.Client({
-  host: "localhost:9200",
-  log: "trace"
+    host: "localhost:9200",
+    log: "trace"
 });
 
 client.search({
-  index: 'customer',
-  body: {
-    query: {
-      match: {
-        name: 'doe'
-      }
+    index: 'customer',
+    body: {
+        query: {
+            match: {
+                name: 'doe'
+            }
+        }
     }
-  }
 }, (err, result) => {
-  if (err) console.log(err)
-  if (result) console.log(result);
+    if (err) console.log(err)
+    if (result) console.log(result);
 })
 
 // define a route handler for the default home page
 app.get("/", (req, res) => {
-  type ola = {
-    [key: string]: number;
-  };
-  console.log(client);
-  res.send("Hello world!");
+    res.send("Hello world!");
+});
+
+app.get('/api/:index', (req, res) => {
+    const index = req.params.index;
+    client.search({
+        index: index,
+        body: {
+            query: {
+                match: {
+                    name: 'doe'
+                }
+            }
+        }
+    }, (err, result) => {
+        res.send(err || result)
+    });
 });
 
 // start the Express server
 app.listen(port, () => {
-  console.log(`server started at http://localhost:${port}`);
+    console.log(`server started at http://localhost:${port}`);
 });
