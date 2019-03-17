94860b2c5aede577e6e5b1de5d1e9d50b7ec14e6
jocta.torres <jocta.torres@gmail.com>
2019-03-17 16:40:26 -0300

    adding more fields

--- front/src/pages/home/index.tsx
+++ front/src/pages/home/index.tsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import React from "react";
 
-const Home = () => <h1>Home page</h1>
+const Home = () => {
+  return (
+    <React.Fragment>
+      <h1>Home page</h1>
+      <form action="">
+        Busca: <input type='text' />
+      </form>
+      <h2>Categoria</h2>
+    </React.Fragment>
+  );
+};
 
-
-export default Home
+export default Home;
