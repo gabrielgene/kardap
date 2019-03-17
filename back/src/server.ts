bf8f44b27d7761500d848859fa8c12c268878d30
ravi <rafael.vitor@jusbrasil.com.br>
2019-03-17 18:59:29 -0300

    Fix get route

--- back/src/server.ts
+++ back/src/server.ts
@@ -5,58 +5,49 @@ const app = express();
 const port = 8080; // default port to listen
 
 const client = new elasticsearch.Client({
-	host: "localhost:9200",
-	log: "trace"
+  host: "localhost:9200",
+  log: "trace"
 });
-
-client.search({
-	index: 'customer',
-	body: {
-		query: {
-			match: {
-				name: 'doe'
-			}
-		}
-	}
-}, (err, result) => {
-	if (err) console.log(err)
-	if (result) console.log(result);
-})
 
 // define a route handler for the default home page
 app.get("/", (req, res) => {
-	res.send("Hello world!");
+  res.send("Hello world!");
 });
 
-app.get('/api/:index', async (req, res) => {
-	const index = req.params.index;
-	const requestQuery = req.query;
-	const queryObject = {
-		index,
-	};
+app.get("/api/:index", async (req, res) => {
+  const index = req.params.index;
+  const requestQuery = Object.keys(req.query).length ? req.query : false;
+  const queryObject = {
+    index,
+  };
 
-	const fullQueryObject = {
-		...queryObject,
-		body: requestQuery
-	};
+  console.log({requestQuery})
 
-	try {
-		const response = await client.search(requestQuery ? fullQueryObject : queryObject, (err, result) => {
-			return (err || result.hits && result.hits.hits)
-		});
-		res.send({
-			message: 'ok',
-			json: response
-		});
-	} catch (err) {
-		res.send({
-			message: 'ok',
-			json: err
-		});
-	}
+  const fullQueryObject = requestQuery ? {
+  	...queryObject,
+  	body: { query: { match: requestQuery } },
+  } : queryObject;
+
+  console.log({fullQueryObject});
+
+  try {
+    const response = await client.search(fullQueryObject)
+    // const response = await client.search(queryObject)
+
+    console.log(response)
+    res.send({
+      message: "ok",
+      json: response.hits ? response.hits.hits : []
+    });
+  } catch (err) {
+    res.send({
+      message: "ok",
+      json: err
+    });
+  }
 });
 
 // start the Express server
 app.listen(port, () => {
-	console.log(`server started at http://localhost:${port}`);
+  console.log(`server started at http://localhost:${port}`);
 });
