2e53675f41457feea17db2a771fa58b0774751d0
Joctã Torres <jocta.torres@gmail.com>
2019-03-17 16:41:52 -0300

    [Front][Home] Adding home poc page (#5)
    * Adding home page h1
    
    * adding more fields

---
+++ front/src/pages/home/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <React.Fragment>
+      <h1>Home page</h1>
+      <form action="">
+        Busca: <input type='text' />
+      </form>
+      <h2>Categoria</h2>
+    </React.Fragment>
+  );
+};
+
+export default Home;
