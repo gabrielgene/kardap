33c6489d852ed9e9fcef7291159aeb00f36fec6f
Ramon Moraes <ramon.moraes94@gmail.com>
2019-03-17 16:39:39 -0300

    sever to ts

--- back/src/server.ts
+++ back/src/server.ts
@@ -1,9 +1,13 @@
-const express = require( "express" );
+import express from "express";
 const app = express();
 const port = 8080; // default port to listen
 
 // define a route handler for the default home page
 app.get( "/", ( req, res ) => {
+    type ola = {
+        [key: string]: number
+    };
+
     res.send( "Hello world!" );
 } );
 
